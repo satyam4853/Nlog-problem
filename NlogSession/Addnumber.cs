c25ec23f5b16340cf02051f8f134c0919a669d44
Your Name <you@example.com>
2021-08-11 15:07:34 +0530

    Nlog programme

---
+++ NlogSession/Addnumber.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NLog;
+
+namespace NlogSession
+{
+    class Addnumber
+    {
+        private readonly Nlog nLog = new Nlog();
+        public int Sum(int firstNumber, int secondNumber)
+        {
+            if (firstNumber == 0 || secondNumber == 0)
+            {
+                nLog.LogDebug("Debug Successfull : Sum()");
+                nLog.LogError("Expecting null values");
+                nLog.LogWarn("firstNumber or secondNumber should not equal to 0");
+            }
+            int result = firstNumber + secondNumber;
+            nLog.LogDebug("Debug Successfull : Sum()");
+            nLog.LogInfo("Sum Method passed, Result" + result);
+            return result;
+        }
+
+
+    }
+}
+
---
+++ NlogSession/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace NlogSession
+{
+    class Program
+    {
+        private static string result;
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Addition on numbers \n Enter a,b values : " + result);
+            int a = Convert.ToInt32(Console.ReadLine());
+            int b = Convert.ToInt32(Console.ReadLine());
+            var add = new Addnumber();
+            Console.WriteLine("Result is " + " " + add.Sum(a, b));
+
+        }
+    }
+}
